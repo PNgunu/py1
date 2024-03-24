31bd7812b40add7164b445f5df8f4214577a3785
PNgunu <160312209+PNgunu@users.noreply.github.com>
2024-03-24 11:27:59 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,14 @@
+# Ask user's name
+name = input("What is your name?")
+# Ask user's age
+age = input("How old are you?")
+# Ask user's location
+location = input("Where do you located?")
+# print personalized message
+print("Hello {},you are {} years old and live in {}.".format(name, age, location))
+
+
+
+
+
+
